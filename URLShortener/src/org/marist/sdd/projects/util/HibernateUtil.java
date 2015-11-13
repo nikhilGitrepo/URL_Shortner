c401451d4e46a39cb939eef06b95803b0d1e8bb3
nikhilGitrepo <nikhil.hiremath1@marist.edu>
2015-11-12 23:30:24 -0500

    Database transactions

---
+++ URLShortener/src/org/marist/sdd/projects/util/HibernateUtil.java
@@ -0,0 +1,24 @@
+package org.marist.sdd.projects.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class HibernateUtil {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	 
+    protected Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+ 
+    public void persist(Object entity) {
+        getSession().persist(entity);
+    }
+ 
+    public void delete(Object entity) {
+        getSession().delete(entity);
+    }
+	
+}
